047365d32452d6afa164629505061a47928b3776
noony9 <noony9@users.noreply.github.com>
2019-11-17 02:40:41 +0300

    progress

--- Program.cs
+++ Program.cs
@@ -6,37 +6,68 @@ using System.Threading.Tasks;
 
 namespace Tic_Tac_Toe
 {
+    // set game conditions
+    enum Condition
+    {
+        Continue,
+        Win,
+        Draw
+    }
     class Program
     {
+        private int[,] GameArray() { }
+        public static void Introduction() // Step 1 greet players and prompt to begin
+        {
+            Console.WriteLine("Greetings!  Welcome to Noony's Tic Tac Toe Game!"); // greet user
+            Console.WriteLine("Press any key to begin."); // prompt user to begin game
+            Console.ReadLine(); // get user input
+        }
+        public static void GameBoard(int[,] array) // Step 2 display current game board
+        {
+            // create the game board using characters for lines, display 1 through 8 as the current indicies of user input array
+            Console.Clear(); // refresh screen
+            Console.WriteLine("       |      |       ");
+            Console.WriteLine($"   {0}   |   {1}  |  {2}   ", array[0, 0], array[0, 1], array[0, 2]);
+            Console.WriteLine($" _____________________  \n");
+            Console.WriteLine($"   {3}   |   {4}  |  {5}   ", array[1, 0], array[1, 1], array[1, 2]);
+            Console.WriteLine($" _____________________  \n");
+            Console.WriteLine($"   {6}   |   {7}  |  {8}   ", array[2, 0], array[2, 1], array[2, 2]);
+            Console.WriteLine($"       |      |       ");
+        }
+        
         static void Main(string[] args)
         {
             // Task: create a tic-tac-toe game
-
-            // Step 1: declare and initialize variables to hold game numbers 0 through 8 (e.g., int zero = 0, int one = 1)
-            int zero = 0, one = 1, two = 2, three = 3, four = 4, five = 5, six = 6, seven = 7, eight = 8;
-
-            // Step 2: declare a 2D array with three rows and 3 columns (indices) and assign variable zero to index 0, one to index 1 and so on
-            string[,] ticArray = new string[3, 3]
+            // Step - initialize game
+                
+            int[,] ticArray = new int[3, 3] // declare a 2D array with three rows and 3 columns (indices) to store user inputs
             {
-                {"zero", "one", "two" }, // row zero
-                {"three", "four", "five"}, // row one
-                {"six", "seven", "eight"} // row two
+                {0, 1, 2 }, // row zero
+                {3, 4, 5 }, // row one
+                {6, 7, 80 } // row two
             };
 
-            // Step 3: create the game board using "|" for lines and 0-8 displayed as indices "one" through "eight"
+            // Step - greet players and prompt to begin
+            Introduction();
 
-            Console.WriteLine("       |      |       " );
-            Console.WriteLine($"   {0}   |   {1}  |  {2}   ", ticArray[0, 0], ticArray[0, 1], ticArray[0, 2]);
-            Console.WriteLine($" _____________________  \n" );
-            Console.WriteLine($"   {3}   |   {4}  |  {5}   ", ticArray[1, 0], ticArray[1, 1], ticArray[1, 2]);
-            Console.WriteLine($" _____________________  \n" );
-            Console.WriteLine($"   {6}   |   {7}  |  {8}   ", ticArray[2, 0], ticArray[2, 1], ticArray[2, 2]);
-            Console.WriteLine($"       |      |       " );
+            // Step - display game board
+            GameBoard(ticArray);
 
-            // Step 4: greet players and prompt to begin
-            Console.WriteLine("Greetings!  Welcome to Noony's Tic Tac Toe Game!");
-            Console.WriteLine("Press any key to begin.");
-            Console.ReadLine();
+            // Step - user 1 turn ChangeTurn()
+
+            // Step - update game board & evaluate game conditions (continue, win or draw)
+            // Step -user 2 turn
+            // Step - update and display game board
+            // Step - continue alternating turns until winning condition is met
+            // Step - announce winner and update score tally
+            // Step - ask users to play again (if no, exit game) (if yes, restart game)
+
+
+
+
+
+
+
 
             // Step 5: prompt user for input on number selection
 
@@ -53,5 +84,6 @@ namespace Tic_Tac_Toe
 
             // Step 10: exit or restart game
         }
+
     }
 }
